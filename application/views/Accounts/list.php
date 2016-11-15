fc4e32e61c89d1756a3ba0766db0d7094333053a
sahel32 <sahel.keyhan@gmail.com>
2016-11-15 16:21:15 +0100

    now all files added

---
+++ application/views/Accounts/list.php
@@ -0,0 +1,97 @@
+ <div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                     <h2>لیست افراد</h2>   
+                        <h5>در جدول پایین شما میتوانید لیست راننده ها را مشاهده کنید.</h5>   
+                    </div>
+                </div>
+                 <!-- /. ROW  -->
+                 <hr />
+                <div class="row">
+                    <div class="col-md-12">
+                        <!-- Advanced Tables -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                             لیست راننده ها
+                        </div>
+                        <div class="panel-body">
+                            <div class="table-responsive">
+                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                    <thead>
+                                        <tr>
+                                            <th>کد</th>
+                                            <th>نام</th>
+                                            <th>تخلص</th>
+                                            <th>بردگی</th>
+                                            <th>رسیدگی</th>
+                                            <th>بیلانس (الباقی)</th>
+                                            <th>تغییرات</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr class="odd gradeX">
+                                            <td>Trident</td>
+                                            <td>Internet Explorer 4.0</td>
+                                            <td>Win 95+</td>
+                                            <td class="center">4</td>
+                                            <td class="center">X</td>
+                                            <td class="center">4</td>
+                                            <td class="center">X</td>
+                                        </tr>
+                                        <tr class="even gradeC">
+                                            <td>Trident</td>
+                                            <td>Internet Explorer 5.0</td>
+                                            <td>Win 95+</td>
+                                            <td class="center">5</td>
+                                            <td class="center">C</td>
+                                            <td class="center">4</td>
+                                            <td class="center">X</td>
+                                        </tr>
+                                        <tr class="odd gradeA">
+                                            <td>Trident</td>
+                                            <td>Internet Explorer 5.5</td>
+                                            <td>Win 95+</td>
+                                            <td class="center">5.5</td>
+                                            <td class="center">A</td>
+                                            <td class="center">4</td>
+                                            <td class="center">X</td>
+                                        </tr>
+                                        <tr class="even gradeA">
+                                            <td>Trident</td>
+                                            <td>Internet Explorer 6</td>
+                                            <td>Win 98+</td>
+                                            <td class="center">6</td>
+                                            <td class="center">A</td>
+                                            <td class="center">4</td>
+                                            <td class="center">X</td>
+                                        </tr>
+                                        <tr class="odd gradeA">
+                                            <td>Trident</td>
+                                            <td>Internet Explorer 7</td>
+                                            <td>Win XP SP2+</td>
+                                            <td class="center">7</td>
+                                            <td class="center">A</td>
+                                            <td class="center">4</td>
+                                            <td class="center">X</td>
+                                        </tr>
+                                        <tr class="even gradeA">
+                                            <td>Trident</td>
+                                            <td>AOL browser</td>
+                                            <td>Win XP</td>
+                                            <td class="center">6</td>
+                                            <td class="center">A</td>
+                                            <td class="center">4</td>
+                                            <td class="center">X</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <!--End Advanced Tables -->
+                    </div>
+                </div>
+                 <!-- /. ROW  -->
+            </div>
+             <!-- /. PAGE INNER  -->
---
+++ application/views/template/footer.php
@@ -0,0 +1,24 @@
+    
+            </div>
+             <!-- /. PAGE INNER  -->
+        </div>
+         <!-- /. PAGE WRAPPER  -->
+    </div>
+     <!-- /. WRAPPER  -->
+    <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+    <!-- JQUERY SCRIPTS -->
+
+     <script src="<?php echo asset_url('js/jquery-1.10.2.js'); ?>" ></script>
+      <!-- BOOTSTRAP SCRIPTS -->
+
+    <script src="<?php echo asset_url('js/bootstrap.min.js'); ?>" ></script>
+    <!-- METISMENU SCRIPTS -->
+
+    <script src="<?php echo asset_url('js/jquery.metisMenu.js'); ?>" ></script>
+      <!-- CUSTOM SCRIPTS -->
+
+    <script src="<?php echo asset_url('js/custom.js'); ?>" ></script>
+    
+   
+</body>
+</html>
